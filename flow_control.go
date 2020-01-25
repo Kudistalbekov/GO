80aa69ee409fdcf4ac431562efcbf6e32c8e7a87
Kudistalbekov <46006047+Kudistalbekov@users.noreply.github.com>
2020-01-25 22:57:55 +0200

    Add files via upload

---
+++ flow_control.go
@@ -0,0 +1,78 @@
+package main
+
+func main() {
+	/*
+		for i := 0; i < 10; i++ {
+			println(" ", i)
+		}
+
+	*/
+	/*
+		var i int = 0
+
+		for i < 10 {
+			println(" ", i)
+			i++
+		}
+	*/
+
+	/*
+		var i, j, k int = 5, 6, 4
+		if j > i {
+			if j > k {
+				fmt.Println("J is the greatest!!")
+			} else {
+				fmt.Println("K is the gretaest!!")
+			}
+		} else {
+			if i > k {
+				fmt.Println("I is the greatest!!")
+			} else {
+				fmt.Println("K is the gretaest!!")
+			}
+		}
+
+		if o := 0; o < 10 {
+			println(" ", o)
+		}
+	*/
+	///----------------------------------------------------------------
+	/*
+		switch o := "two"; o {
+		case "one":
+			fmt.Println("1")
+		case "two":
+			fmt.Println(2)
+		case "three":
+			fmt.Println("3")
+		case "four":
+			fmt.Println("4")
+		case "five":
+			fmt.Println("5")
+		}
+		//-----------------------------------------------------------------
+		o := "kudi"
+		switch {
+
+		case o == "kudi":
+			fmt.Println("Kudi")
+		case o == "Ali":
+			fmt.Println("Ali")
+		case o == "Beks":
+			fmt.Println("Beks")
+
+		}
+	*/
+	//-----------------------------------------------------------------
+
+	//DEFERED FUNCTIONS DROPPED INTO THE STACK LAST IN FIRST OUT
+	/*
+		defer fmt.Println("one")
+		fmt.Println("two")
+		defer fmt.Println("Three")
+		defer fmt.Println("four")
+		fmt.Println("five")
+		defer fmt.Println("six")
+	*/
+
+}
